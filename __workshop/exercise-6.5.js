eddf9909181a5d6b71e4e44aa7be4fcb5e00bdce
Doug Wright <dw@nof49.ca>
2020-09-19 16:24:25 -0400

    Completed exercise 6.5

--- __workshop/exercise-6.5.js
+++ __workshop/exercise-6.5.js
@@ -16,6 +16,14 @@ function exercise6_5() {
   //
   // Write code between the lines (below)
   // -----------------------------------------------------------------
+  for (let x = 0; x <= 10; x++) {
+    
+    if (x % 2 === 0) {
+      console.log(`${x} is even`);
+    } else {
+      console.log(`${x} is odd`);
+    }
+  }
   // -----------------------------------------------------------------
   // Write code between the lines (above)
   //
