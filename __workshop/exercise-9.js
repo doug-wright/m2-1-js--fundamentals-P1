18c0d059bce7a905b27ab46f22a0c348e2550543
Doug Wright <dw@nof49.ca>
2020-09-21 16:21:06 -0400

    Completed exercise 9

--- __workshop/exercise-9.js
+++ __workshop/exercise-9.js
@@ -28,7 +28,7 @@ function exercise9() {
   //
   // Write code between the lines (below)
   // -----------------------------------------------------------------
-    for (let x = 1; x <=100; x++) {
+    for (let x = 1; x <= 100; x++) {
       if (x % 3 === 0 && x % 5 === 0) {
         console.log("FizzBuzz");
       } else if (x % 3 === 0) {
